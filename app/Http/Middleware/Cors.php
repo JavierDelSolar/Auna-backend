2e014bef6a7004f45d373c7bd26c79ed0ae89516
JavierDelSolar <jadc.100890@gmail.com>
2021-10-27 17:48:11 -0500

    Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $http_origin = $_SERVER['HTTP_ORIGIN'];
-        $origins = array('http://localhost:4200/', 'http://localhost/', 'http://35.173.151.159/');
+        $origins = array('http://localhost:4200', 'http://localhost', 'http://35.173.151.159');
         $http_origin = in_array($http_origin, $origins)?$http_origin:'';
         return $next($request)
             ->header("Access-Control-Allow-Origin", $http_origin)
